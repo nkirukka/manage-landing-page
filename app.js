5573636b8d54a627377f29b4530947757d967256
Nkiruka Ebele <specialebele@gmail.com>
2022-08-10 00:11:03 +0100

    Add more styles

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const menu = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
+const overlay = document.querySelector(".overlay");
+const slide = document.querySelector("#slide");
+
 
 burger.addEventListener("click", () =>{
     burger.classList.toggle("active");
     menu.classList.toggle("active");
+    // overlay.classList.toggle("active");
 })
+// slide.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     return false
+// })
